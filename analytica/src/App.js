cce87045f9855c481f6d644065f808e183b78ca9
RikeshThapa <rthapa@u.rochester.edu>
2020-10-06 11:15:56 -0500

    Again I need a gitignore, but imported Antd for tables, implemented table with filter and sort feature, all with dummy data. Also added a basic grid layout to align table and chart

--- analytica/src/App.js
+++ analytica/src/App.js
@@ -1,13 +1,103 @@
 import React from 'react';
-import Horizontal_Stacked from './charts/components/Horizontal_Stacked'
+import ReactDOM from 'react-dom';
+import { Table, Row, Col } from 'antd';
+
+import Horizontal_Stacked from './charts/components/Horizontal_Stacked';
+
 import './App.css';
+import 'antd/dist/antd.css';
 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Chinese Score',
+    dataIndex: 'chinese',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: 'Math Score',
+    dataIndex: 'math',
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: 'English Score',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+];
 
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
+
+function onChange(pagination, filters, sorter, extra) {
+  console.log('params', pagination, filters, sorter, extra);
+}
 
 function App() {
   return (
     <div className="App">
-      <Horizontal_Stacked />       
+      <Row gutter={[16,16]}>
+        <Col span={12}>
+          <Table
+            columns={columns}
+            dataSource={data}
+            onChange={onChange}
+            onRow={(record, rowIndex) => {
+              return {
+                onClick: event => {}, // click row
+              };
+            }}
+            onHeaderRow={column => {
+              return {
+                onClick: () => {}, // click header row
+              };
+            }} 
+          />
+        </Col>
+        <Col span={12}>
+          <Horizontal_Stacked />
+        </Col>
+      </Row>
     </div>
   );
 }
