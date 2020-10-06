349a64d6f57a906dc23b74dd4956d343b84a0dfd
RikeshThapa <rthapa@u.rochester.edu>
2020-10-05 20:59:39 -0500

    creating a base bar graph, starting dashboard form here

--- analytica/src/App.js
+++ analytica/src/App.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+
 import './App.css';
+
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
+
+const data2013 = [
+  {quarter: 1, earnings: 15000},
+  {quarter: 2, earnings: 12500},
+  {quarter: 3, earnings: 19500},
+  {quarter: 4, earnings: 13000}
+];
+
+const data2012 = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
+
+const data2014 = [
+  {quarter: 1, earnings: 11500},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 20000},
+  {quarter: 4, earnings: 15500}
+];
+
+const data2015 = [
+  {quarter: 1, earnings: 18000},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 15000},
+  {quarter: 4, earnings: 12000}
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <VictoryChart
+          domainPadding={20}
+          theme={VictoryTheme.material}
+          className = "chart"
         >
-          Learn React
-        </a>
-      </header>
+            <VictoryAxis
+              tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+            />
+            <VictoryAxis
+              dependentAxis
+              tickFormat={(x) => (`$${x / 1000}k`)}
+            />
+                <VictoryStack
+                  colorScale={"warm"}
+                >
+                <VictoryBar
+                data={data2012}
+                x="quarter"
+                y="earnings"
+              />
+              <VictoryBar
+                data={data2013}
+                x="quarter"
+                y="earnings"
+              />
+              <VictoryBar
+                data={data2014}
+                x="quarter"
+                y="earnings"
+              />
+              <VictoryBar
+                data={data2015}
+                x="quarter"
+                y="earnings"
+              />
+            </VictoryStack>
+        </VictoryChart>
     </div>
   );
 }
